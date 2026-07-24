76e04b81db489f72a721478315b1193d148fb7d5
ryawa <22625346+ryawa@users.noreply.github.com>
2026-07-24 19:41:55 -0400

    Improve English text scoring function

--- src/crypto/utils.py
+++ src/crypto/utils.py
@@ -1,36 +1,39 @@
+from math import log10
+
+# From https://en.wikipedia.org/wiki/Letter_frequency
 LETTER_FREQ = {
-    "A": 7.8,
-    "B": 2.0,
-    "C": 4.0,
-    "D": 3.8,
-    "E": 11.0,
-    "F": 1.4,
-    "G": 3.0,
-    "H": 2.3,
-    "I": 8.6,
-    "J": 0.25,
-    "K": 0.97,
-    "L": 5.3,
-    "M": 2.7,
-    "N": 7.2,
-    "O": 6.1,
-    "P": 2.8,
-    "Q": 0.19,
-    "R": 7.3,
-    "S": 8.7,
-    "T": 6.7,
-    "U": 3.3,
-    "V": 1.0,
-    "W": 0.91,
-    "X": 0.27,
-    "Y": 1.6,
-    "Z": 0.44,
+    "A": 8.2,
+    "B": 1.5,
+    "C": 2.8,
+    "D": 4.3,
+    "E": 12.7,
+    "F": 2.2,
+    "G": 2.0,
+    "H": 6.1,
+    "I": 7.0,
+    "J": 0.16,
+    "K": 0.77,
+    "L": 4.0,
+    "M": 2.4,
+    "N": 6.7,
+    "O": 7.5,
+    "P": 1.9,
+    "Q": 0.12,
+    "R": 6.0,
+    "S": 6.3,
+    "T": 9.1,
+    "U": 2.8,
+    "V": 0.98,
+    "W": 2.4,
+    "X": 0.15,
+    "Y": 2.0,
+    "Z": 0.074,
 }
 
-def score(s: bytes, freq: dict[str, float]=LETTER_FREQ, penalty: float=-10):
+def score(s: bytes, freq: dict[str, float]=LETTER_FREQ, penalty: float=-1):
     result = 0
     for c in s.decode("latin-1"):
-        result += freq.get(c.upper(), penalty)
+        result += log10(freq.get(c.upper(), 10**penalty))
     return result
 
 def hamming_dist(a: bytes, b: bytes) -> int:
